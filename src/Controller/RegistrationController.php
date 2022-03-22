38594887c16724979282130d76c38cbec7daef50
cedriccaijo <49797665+cedriccaijo@users.noreply.github.com>
2022-03-22 12:44:49 +0100

    return string
    string

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,9 +12,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class RegistrationController extends AbstractController
-{
+{   
+    private $serializer;
+
+    public function __construct(
+        SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
     /**
      * @Route("/register", name="app_register")
      */
@@ -35,9 +46,24 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email         
+            // do anything else you need here, like send an email 
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
         }
-        return $this->json('test');
+        $result = $this->serializer->serialize(
+            $user,
+            'json',
+            [
+                AbstractNormalizer::ATTRIBUTES =>
+                    ['id', 'email', 'roles', 'posted'
+                    ]
+            ]
+        );
+        return new JsonResponse($result, 200, [], true);
     }
         
 }
